15b6dc75f07f1f253a09464460011fb19a66b0ca
Avinash Singh <avinash.singh01@nagarro.com>
2024-03-11 11:50:10 +0530

    Added Category Crud in Razor project & Added toastr Notification

--- E-Commerce.RazorProject/Pages/Categories/Index.cshtml.cs
+++ E-Commerce.RazorProject/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using E_Commerce.RazorProject.Data;
+using E_Commerce.RazorProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,15 @@ namespace E_Commerce.RazorProject.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;    
+        }
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
