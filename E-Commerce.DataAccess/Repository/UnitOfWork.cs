d48f7a782a89911ace46831bf1deb8e0ba241edf
Avinash Singh <avisingh332@gmail.com>
2024-03-13 11:17:53 +0530

    Updated UnitofWork to implement Lazy Initialization of Category and Product

--- E-Commerce.DataAccess/Repository/UnitOfWork.cs
+++ E-Commerce.DataAccess/Repository/UnitOfWork.cs
@@ -1,20 +1,30 @@
 ﻿using E_Commerce.DataAccess.Repository.IRepository;
 using E_Commerce.DataAccess.Data;
-
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace E_Commerce.DataAccess.Repository
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _db;
-        public ICategoryRepository Category { get; private set; }
-        public IProductRepository Product { get; private set; } 
+        private readonly Lazy<ICategoryRepository> _categoryRepository;
+        private readonly Lazy<IProductRepository> _productRepository;
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
-            Category = new CategoryRepository(_db);
-            Product = new ProductRepository(_db);   
+            _categoryRepository = new Lazy<ICategoryRepository>(new CategoryRepository(_db));
+            _productRepository = new Lazy<IProductRepository>(new ProductRepository(_db));  
         }
+
+        public ICategoryRepository Category
+        {
+            get { return _categoryRepository.Value; }
+        }
+        public IProductRepository Product
+        {
+            get { return _productRepository.Value;}
+        }
+
         public void Save()
         {
            _db.SaveChanges();
