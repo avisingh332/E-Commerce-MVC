9d15cfdc81cf15092f9f3c0bd7cb2629d8812001
Avinash Singh <avisingh332@gmail.com>
2024-03-15 17:50:42 +0530

    Added Index and Details page for Home

--- E-Commerce Application/Areas/Customer/Controllers/HomeController.cs
+++ E-Commerce Application/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using E_Commerce_Application.Models;
+﻿using E_Commerce.DataAccess.Repository.IRepository;
+using E_Commerce_Application.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,17 +9,24 @@ namespace E_Commerce_Application.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var productList = _unitOfWork.Product.GetALL(includeProperties: "Category").ToList();
+            return View(productList);
         }
-
+        public IActionResult Details(int productId)
+        {
+            var product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
